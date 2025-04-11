7166206c567dea9f58eadd857137aa8383fd2584
Fedor Ptitsyn <fedor.ptith@gmail.com>
2025-04-11 20:39:49 +0300

    Basic player with movement and first person camera

---
+++ Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class Controller : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed;
+        [SerializeField] private float mouseSensitivity;
+        
+        private CharacterController _characterController;
+        private Transform _cameraTransform;
+
+        private void Start()
+        {
+            _characterController = GetComponent<CharacterController>();
+            _cameraTransform = Camera.main.transform;
+            _cameraTransform.position = new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z);
+            _cameraTransform.parent = transform;
+        }
+
+        private void Update()
+        {
+            HandleInput();
+        }
+
+        private void HandleInput()
+        {
+            float moveX = Input.GetAxis("Horizontal") * moveSpeed;
+            float moveY = Input.GetAxis("Vertical") * moveSpeed;
+            Vector3 movement = transform.right * moveX + transform.forward * moveY;
+            
+            _characterController.Move(movement * Time.deltaTime);
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+            transform.Rotate(Vector3.up * mouseX);
+            _cameraTransform.Rotate(Vector3.left * mouseY);
+        }
+    }
+}
