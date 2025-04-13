fb7c9fbe46c1688a6537a2a30f24339c9c673030
Niston <vadimpavlov@bk.ru>
2025-04-13 16:53:23 +0300

    Fader

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,3 +1,4 @@
+using GameControl;
 using Settings.Audio;
 using UnityEditor;
 using UnityEngine;
@@ -16,6 +17,9 @@ namespace UI
 
         [SerializeField] private GameObject controlButtons;
         [SerializeField] private GameObject settingsUI;
+
+        private FaderExample _fader;
+        
         
         private void Awake()
         {
@@ -27,6 +31,7 @@ namespace UI
 
         private void Start()
         {
+            _fader = FindFirstObjectByType<FaderExample>();
             AudioManager.instance.PlayMusic("MainMenu");
             AudioManager.instance.PlayAmbient("AmbientMenu");
             settingsUI.SetActive(false);
@@ -35,7 +40,7 @@ namespace UI
         private void PlayGame()
         {
             AudioManager.instance.PlaySfx("Click");
-            SceneManager.LoadScene(1);
+            _fader.LoadScene("Room");
             AudioManager.instance.PlayMusic("ChestOpening");
             AudioManager.instance.PlayAmbient("Ambient");
         }
